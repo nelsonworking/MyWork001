0321ceef1b8d43ea34ef25956a096d98cee94502
Nelson Huang <nelson.working@gmail.com>
2014-12-18 11:53:42 +0800

    測試 memcache

---
+++ Test/Default2.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default2.aspx.cs" Inherits="Test_Default2" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <br />
+        <br />
+        <asp:DropDownList ID="DropDownList1" runat="server">
+            <asp:ListItem>tw</asp:ListItem>
+            <asp:ListItem>en</asp:ListItem>
+        </asp:DropDownList>
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+        <br />
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Test/Default3.aspx.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using MemcachedProviders.Cache;
+
+[Serializable]
+public class UU {
+    public string name = "";
+    
+    public  UU(string _name) {
+        this.name = _name;
+    }
+}
+
+
+public partial class Test_Default3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+       string cacheKey = "myname";
+       string cacheValue = "Nelson";
+
+       UU u1 = new UU("nelson");
+
+       DistCache.Add("U1", u1);
+
+
+
+        //object objCache = DistCache.Get(cacheKey);
+        
+        // 寫入快取資料 (預設過期時間)
+        DistCache.Add(cacheKey, cacheValue);
+        // 快取 60 秒
+        DistCache.Add(cacheKey, cacheValue, 60 * 1000);
+        
+        // 快取至今天結束
+        DistCache.Add(cacheKey, cacheValue, DateTime.Today.AddDays(1) - DateTime.Now);
+
+
+
+        
+        
+
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+         //string cacheKey = "myname";
+        //object objCache = DistCache.Get(cacheKey);
+        //Label1.Text = objCache.ToString();
+
+
+        UU u1 =(UU) DistCache.Get("U1");
+        Label1.Text = u1.name;
+
+
+    }
+}
